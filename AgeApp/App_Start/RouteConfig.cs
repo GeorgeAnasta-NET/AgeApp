c940bb92ded81258e1589ac02d27e64bfb55b759
George Anastasiadis <anastasiadisgeo87@gmail.com>
2021-10-24 23:34:28 +0300

    Create Route Config for paths

--- AgeApp/App_Start/RouteConfig.cs
+++ AgeApp/App_Start/RouteConfig.cs
@@ -10,6 +10,8 @@ namespace AgeApp {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
