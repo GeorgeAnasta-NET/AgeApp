2da0ddffdbf97213af1ef4ee8c8cea62e70c6ecf
George Anastasiadis <anastasiadisgeo87@gmail.com>
2021-10-24 21:47:01 +0300

    Add project files.

---
+++ AgeApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AgeApp.MvcApplication" Language="C#" %>
---
+++ AgeApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AgeApp.Startup))]
+namespace AgeApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
