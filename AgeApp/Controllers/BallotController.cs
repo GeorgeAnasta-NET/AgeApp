627449c5af7a2a10f42e509417307f3292ecb0bd
George Anastasiadis <anastasiadisgeo87@gmail.com>
2021-10-24 23:36:50 +0300

    Pass and View ViewModel

--- AgeApp/Controllers/BallotController.cs
+++ AgeApp/Controllers/BallotController.cs
@@ -1,4 +1,5 @@
 ﻿using AgeApp.Models;
+using AgeApp.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,8 +14,37 @@ namespace AgeApp.Controllers
         public ActionResult Random()
         {
             var ballot = new Ballot() { Name = "Shrek!" };
+            var voters = new List<Voter> {
+                new Voter{Name = "Voter1"},
+                new Voter{Name = "Voter 2 "}
+            };
 
-            return View(ballot);
+            var viewModel = new RandomViewModel {
+                Ballot = ballot,
+                Voters = voters
+            };
+
+
+            return View(viewModel);
         }
+
+        public ActionResult Edit(long Id) {
+            return Content("Id = " + Id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy) {
+            if (!pageIndex.HasValue) {
+                pageIndex = 1;
+            }
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex = {0} & sortBy = {1}", pageIndex, sortBy));
+        }
+
+        //[Route("Ballot/released/{year}/{month:regerx(\\d{4}):range(1, 12}")]
+        //public ActionResult ByReleaseDate(int year, int month) {
+        //    return Content(year + "/" + month);
+        //}
     }
 }
