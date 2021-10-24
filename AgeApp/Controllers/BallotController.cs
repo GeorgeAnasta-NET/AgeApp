0dc7b551fe20a6f9fb3632a510b82297f39f0175
George Anastasiadis <anastasiadisgeo87@gmail.com>
2021-10-24 22:05:58 +0300

    Example for MVC(with ballot)

---
+++ AgeApp/Controllers/BallotController.cs
@@ -0,0 +1,20 @@
+﻿using AgeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AgeApp.Controllers
+{
+    public class BallotController : Controller
+    {
+        // GET: Ballot/Random
+        public ActionResult Random()
+        {
+            var ballot = new Ballot() { Name = "Shrek!" };
+
+            return View(ballot);
+        }
+    }
+}
