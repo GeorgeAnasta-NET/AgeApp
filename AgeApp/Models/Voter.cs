7962fbee1ccd0477dbf1f69d320bb7b6b4390c91
George Anastasiadis <anastasiadisgeo87@gmail.com>
2021-10-24 23:35:52 +0300

    Create ViewModel

---
+++ AgeApp/Models/Voter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AgeApp.Models {
+    public class Voter {
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ AgeApp/ViewModel/RandomViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AgeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AgeApp.ViewModel {
+    public class RandomViewModel {
+
+        public Ballot Ballot { get; set; }
+        public List<Voter> Voters { get; set; }
+
+    }
+}
