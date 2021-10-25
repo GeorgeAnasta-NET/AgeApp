c337df082f5bc0fb0a16c9ec045d043bf2dceb9f
George Anastasiadis <anastasiadisgeo87@gmail.com>
2021-10-25 22:35:45 +0300

    Add DbContext to CustomerController

--- AgeApp/Controllers/CustomersController.cs
+++ AgeApp/Controllers/CustomersController.cs
@@ -8,27 +8,30 @@ using System.Web.Mvc;
 namespace AgeApp.Controllers
 {
     public class CustomersController : Controller {
-        public ViewResult Index() {
-            var customers = GetCustomers();
+        private ApplicationDbContext _context;
+
+        public CustomersController() {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing) {
+            _context.Dispose();
+        }
+
+        public ViewResult Index() 
+        {
+            var customers = _context.Voters.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id) {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Voters.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Voter> GetCustomers() {
-            return new List<Voter>
-            {
-                new Voter { Id = 1, Name = "John Smith" },
-                new Voter { Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 } 
