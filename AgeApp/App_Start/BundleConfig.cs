6e61fb5dcb1a6fc3cfda89c455a6de85d5e9a0b1
George Anastasiadis <anastasiadisgeo87@gmail.com>
2022-02-08 23:00:37 +0200

    Create Delete btn with bootbox

--- AgeApp/App_Start/BundleConfig.cs
+++ AgeApp/App_Start/BundleConfig.cs
@@ -17,7 +17,9 @@ namespace AgeApp {
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
